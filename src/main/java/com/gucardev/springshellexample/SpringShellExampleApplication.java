40f1977573bea356511addc05906789a4618937f
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-11-26 18:58:24 +0300

    Initial commit

---
+++ src/main/java/com/gucardev/springshellexample/SpringShellExampleApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.springshellexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShellExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringShellExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gucardev/springshellexample/SpringShellExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucardev.springshellexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShellExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
