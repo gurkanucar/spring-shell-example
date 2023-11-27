c9a4c24bb1ba52ba2e061e31f5fca3a89f48ad74
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-11-27 16:05:36 +0300

    doc: slides added to README.md

--- src/main/java/com/gucardev/springshellexample/exception/CLIExceptionResolver.java
+++ src/main/java/com/gucardev/springshellexample/exception/CLIExceptionResolver.java
@@ -6,17 +6,16 @@ import org.springframework.shell.command.CommandExceptionResolver;
 import org.springframework.shell.command.CommandHandlingResult;
 
 public class CLIExceptionResolver implements CommandExceptionResolver {
-    @Override
-    public CommandHandlingResult resolve(Exception ex) {
-        if (ex instanceof ParameterValidationException e) {
-            return CommandHandlingResult.of(
-                    e.getConstraintViolations()
-                            .stream()
-                            .map(violation -> violation.getPropertyPath() + " " + violation.getMessage())
-                            .collect(Collectors.joining(". "))
-                            +'\n');
-        }
-
-        return CommandHandlingResult.of(ex.getMessage()+'\n', 1);
+  @Override
+  public CommandHandlingResult resolve(Exception ex) {
+    if (ex instanceof ParameterValidationException e) {
+      return CommandHandlingResult.of(
+          e.getConstraintViolations().stream()
+                  .map(violation -> violation.getPropertyPath() + " " + violation.getMessage())
+                  .collect(Collectors.joining(". "))
+              + '\n');
     }
+
+    return CommandHandlingResult.of(ex.getMessage() + '\n', 1);
+  }
 }
