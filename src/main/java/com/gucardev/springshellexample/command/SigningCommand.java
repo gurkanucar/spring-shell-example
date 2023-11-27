bb0cfa86a9378c1b1d1e9db98f96b4e7ed14f133
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-11-27 10:45:28 +0300

    feat: console beautified

--- src/main/java/com/gucardev/springshellexample/command/SigningCommand.java
+++ src/main/java/com/gucardev/springshellexample/command/SigningCommand.java
@@ -1,11 +1,10 @@
 package com.gucardev.springshellexample.command;
 
 import com.gucardev.springshellexample.util.ShellReader;
-import java.io.BufferedReader;
-import java.io.Console;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import lombok.RequiredArgsConstructor;
+import org.fusesource.jansi.Ansi;
+import org.fusesource.jansi.AnsiConsole;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -21,16 +20,18 @@ public class SigningCommand {
   private final AuthenticationManager authenticationManager;
 
   @Command(command = "login", description = "login method")
-  public void login() throws IOException {
-    String username = readInput("Please enter your username: ");
-    String password = readPassword("Please enter your password: ");
+  public void login(
+      //  @Option(shortNames = 'u') String username, @Option(shortNames = 'p') String password
+      ) throws IOException {
+    String username = shellReader.readLine("Please enter your username");
+    String password = shellReader.readLinePassword("Please enter your password");
     Authentication request = new UsernamePasswordAuthenticationToken(username, password);
     try {
       Authentication result = authenticationManager.authenticate(request);
       SecurityContextHolder.getContext().setAuthentication(result);
-      System.out.println("Successfully authenticated!");
+      printColored("Successfully authenticated!", Ansi.Color.GREEN);
     } catch (AuthenticationException e) {
-      System.out.println("Authentication failed: " + e.getMessage());
+      printColored("Authentication failed: " + e.getMessage(), Ansi.Color.RED);
     }
   }
 
@@ -40,18 +41,9 @@ public class SigningCommand {
     System.out.println("You have been logged out.");
   }
 
-  private String readPassword(String prompt) throws IOException {
-    Console console = System.console();
-    if (console == null) {
-      return readInput(prompt);
-    } else {
-      return new String(console.readPassword(prompt));
-    }
-  }
-
-  private String readInput(String prompt) throws IOException {
-    System.out.print(prompt);
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    return reader.readLine();
+  private void printColored(String message, Ansi.Color color) {
+    AnsiConsole.systemInstall();
+    System.out.println(Ansi.ansi().fg(color).a(message).reset());
+    AnsiConsole.systemUninstall();
   }
 }
--- src/main/java/com/gucardev/springshellexample/config/SpringShellConfiguration.java
+++ src/main/java/com/gucardev/springshellexample/config/SpringShellConfiguration.java
@@ -1,13 +1,43 @@
 package com.gucardev.springshellexample.config;
 
 import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStyle;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.shell.jline.PromptProvider;
 
 @Configuration
-public class SpringShellConfiguration implements PromptProvider {
-  @Override
-  public final AttributedString getPrompt() {
-    return new AttributedString("myapp:> ");
+public class SpringShellConfiguration {
+
+  @Bean
+  public PromptProvider promptProvider() {
+    return () -> {
+      AttributedStyle style = determinePromptStyle();
+      String promptString = buildPromptString();
+      return new AttributedString(promptString, style);
+    };
+  }
+
+  private AttributedStyle determinePromptStyle() {
+    if (SecurityContextHolder.getContext().getAuthentication() != null
+        && SecurityContextHolder.getContext().getAuthentication().isAuthenticated()) {
+      return AttributedStyle.DEFAULT.background(AttributedStyle.GREEN);
+    } else {
+      return AttributedStyle.DEFAULT.background(AttributedStyle.RED);
+    }
+  }
+
+  private String buildPromptString() {
+    String username = getUsername();
+    return "myapp" + (username != null ? ":" + username : "") + " > ";
+  }
+
+  private String getUsername() {
+    if (SecurityContextHolder.getContext().getAuthentication() != null
+        && SecurityContextHolder.getContext().getAuthentication().isAuthenticated()) {
+      return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+    return null;
   }
 }
